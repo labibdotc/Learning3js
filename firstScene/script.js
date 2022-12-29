bb1cb0ec5229768734c126c62e01a3dd21fe14e1
Abdulrahman (Labib) Afia <98433990+labibdotc@users.noreply.github.com>
2022-12-28 22:02:53 -0500

    scene pic

--- firstScene/script.js
+++ firstScene/script.js
@@ -13,7 +13,8 @@ const sizes = {
 //camera (pov)
 const camera = new THREE.PerspectiveCamera(75,sizes.width/sizes.height)
 camera.position.z=3
-// camera.position.x=2
+camera.position.x=-1
+camera.position.y=-1
 scene.add(camera) //adds camera to the scene
 
 
